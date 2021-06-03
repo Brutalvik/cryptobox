a552640683a323d908316c2ea748ef75edeaf83f
vikram <vikcanada90@gmail.com>
2021-06-03 17:18:39 -0400

    Extrated Symbols from API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,37 @@ export class HomeComponent implements OnInit {
 
   objectKeys = Object.keys;
   cryptos: any;
-
+  tickers: any;
+  ticker: any;
+  tickerArray = [];
+  key: 'cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce'
+  krypto: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH&tsyms=CAD')
-    .subscribe(result => this.cryptos = result)
+    this.getTickers()
   }
+
+
+
+  getTickers() {
+    this.http.get(`https://min-api.cryptocompare.com/data/blockchain/list?api_key=cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce`)
+    .toPromise().then(result =>
+      { //console.log(result)
+
+        this.cryptos = result["Data"]
+        for (let crypto in this.cryptos)
+        {
+          this.tickers = this.cryptos[crypto]
+          //console.log(this.tickers["symbol"])
+          this.ticker = this.tickers["symbol"]
+          this.tickerArray.push(this.ticker)
+        }
+      });
+
+  }
+
+
 
 }
