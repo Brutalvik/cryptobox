9a7087e1117816673d3da0794378bf84f40dba02
vikram <vikcanada90@gmail.com>
2021-06-04 02:29:50 -0400

    Display API Data Succesful

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormControl} from '@angular/forms';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +13,6 @@ import { FormGroup, FormControl} from '@angular/forms';
 
 export class HomeComponent implements OnInit {
 
-
-
-  tickerApi = 'https://min-api.cryptocompare.com/data/blockchain/list?api_key=cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce'
-  currencyApi = 'https://openexchangerates.org/api/currencies.json'
-
   objectKeys = Object.keys;
   cryptos: any;
   tickers: any;
@@ -24,8 +20,16 @@ export class HomeComponent implements OnInit {
   tickerArray = []; //Array of tickers
   currencyArray = []; //Array of currencies
 
-  selectedCrypto = ""
-  selectedCurrency = ""
+  selectedCrypto = "BTC"
+  selectedCurrency = "CAD"
+  cryptoData: any;
+  cryptoInfo: any;
+  crypto: any;
+  cryptoArray = [];
+
+
+  tickerApi = 'https://min-api.cryptocompare.com/data/blockchain/list?api_key=cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce'
+  currencyApi = 'https://openexchangerates.org/api/currencies.json'
 
   constructor(private http: HttpClient) { }
 
@@ -65,10 +69,24 @@ export class HomeComponent implements OnInit {
   }
 
   displayData(){
-    console.log(this.selectedCrypto.toString())
-    console.log(this.selectedCurrency.toString())
+    this.cryptoArray = []
+    this.http.get(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${this.selectedCrypto}&tsyms=${this.selectedCurrency}`).toPromise()
+    .then(result => {
+      this.cryptoData = result["DISPLAY"]
+      for (let crypto in this.cryptoData)
+      {
+        console.log(crypto)
+        this.cryptoInfo = this.cryptoData[crypto]
+        for (let item in this.cryptoInfo)
+        {
+          console.log(item)
+          this.crypto = this.cryptoInfo[item]
+          this.cryptoArray.push(this.crypto)
+          console.log(this.cryptoArray)
+        }
+      }
+    })
+
   }
-
-
 
 }
