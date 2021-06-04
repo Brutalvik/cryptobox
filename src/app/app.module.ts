369b9c592e0df9ac96f829f4b5ad17f2fd7a23f0
vikram <vikcanada90@gmail.com>
2021-06-04 01:33:10 -0400

    Created Dropdowns for Crypto and Currency Selection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input'
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
 //Forms
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -33,6 +34,7 @@ import { HomeComponent } from './home/home.component';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
+    MatSelectModule,
     ReactiveFormsModule,
     HttpClientModule,
   ],
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,31 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
+
 export class HomeComponent implements OnInit {
+
+
+
+  tickerApi = 'https://min-api.cryptocompare.com/data/blockchain/list?api_key=cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce'
+  currencyApi = 'https://openexchangerates.org/api/currencies.json'
 
   objectKeys = Object.keys;
   cryptos: any;
   tickers: any;
   ticker: any;
-  tickerArray = [];
-  key: 'cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce'
-  krypto: any;
+  tickerArray = []; //Array of tickers
+  currencyArray = []; //Array of currencies
+
+  selectedCrypto = ""
+  selectedCurrency = ""
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getTickers()
+    this.getCurrency()
   }
 
 
+  getCurrency() {
+    this.http.get(this.currencyApi)
+    .toPromise()
+    .then(result => {
+
+      for (let currency in result)
+      {
+          this.currencyArray.push(currency)
+      }
+      // console.log(this.currencyArray[10])
+    })
+  }
 
   getTickers() {
-    this.http.get(`https://min-api.cryptocompare.com/data/blockchain/list?api_key=cea3b6e1d525e7743a9a2d803db00d8518755a1fd9e7df73c126a4911feb36ce`)
+    this.http.get(this.tickerApi)
     .toPromise().then(result =>
       { //console.log(result)
 
@@ -38,7 +62,11 @@ export class HomeComponent implements OnInit {
           this.tickerArray.push(this.ticker)
         }
       });
+  }
 
+  displayData(){
+    console.log(this.selectedCrypto.toString())
+    console.log(this.selectedCurrency.toString())
   }
 
 
